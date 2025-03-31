1e9efdac1e916685285eed51322e9d5f1f74c1d1
alina <alina.meller.98@mail.ru>
2025-03-31 12:30:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# Списки - мультитипная упорядоченная коллекция
+# m1 = 4
+# m2 = 5
+# m3 = 5
+# print(m1 + "")
+# grades = [4, 5, 5, 4, 5, 5, 5]
+# print(grades)
+# print(*grades)
+# names = ['Александр', 'Антон', 'Катя']
+# print(*names)
+# lst1 = ['Hello', 78, False]
+# lst2 = []
+# lst3 = list()
+# lst4 = list('Александр')
+# lst5 = list(range(1, 100))
+# print(lst5)
+# lst6 = []
+# lst7 = '2022-03-12'.split('-')
+# print('2022-03-12'.split())
+# for i in range(1, 101):
+#     lst6.append(i)
+# print(lst4)
+# print(lst2)
+# print(lst3)
+
+# print(range(10))
+# print(range(1, 10))
+# print(range(10, 1, -1))
+
+# Пользователь вводит время в формате 14:35. Разбить на 2 части и распечатать список
+# a=input()
+# print(a.split(':'))
+
+
+# time = ['12', '40']
+#
+# print(time[0])
+
+
+lst = [100, 24, 31, 45, 5, 6, 7, 8, 9, 10]
+# print(lst[0])
+# print(lst[1])
+# print(lst[2])
+# print(lst[3])
+# print(lst[4])
+# print(lst[5])
+# print(lst[6])
+# print(lst[7])
+# print(lst[8])
+# print(lst[9])
+for i in range(0,10):
+    print(lst[i])
