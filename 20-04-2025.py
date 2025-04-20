1ec62e74b0183661f03df030c555aab28a9bcb6b
alina <alina.meller.98@mail.ru>
2025-04-20 15:08:34 +0300

    20-04-2025

---
+++ 20-04-2025.py
@@ -0,0 +1,39 @@
+# lst = [1, 2, 3, 4, 5]
+# for i in range(10):
+#     lst.append(1)
+# print(lst)
+# lst1 = [1, 2, 3] * 10
+# print(lst1)
+
+# print(len(lst))
+# print(max(lst))
+# print(min(lst))
+# print(sum(lst))
+
+# n=int(input())
+# lst = [1, 2, 3, 4, 5]
+# # print(n in lst)
+# # if n not in lst:
+# #     print(n)
+# lst2 = [6, 7, 8, 9, 10]
+# # lst.append(lst2)
+# # 1 способ расширения списка
+# # lst += lst2
+# lst.extend(lst2)
+# print(lst)
+# Строки - неизменяемые
+# line = "Hello"
+# print(line[0:2])
+# line = "dfjkfjks"
+
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(lst[2:5])
+
+print(lst[7:10])
+
+
+
+
+# lst[2] = 100
+# print(lst)
